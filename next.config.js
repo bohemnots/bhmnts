42eca3fe06c40ca5b3308359d9fd2988c2e9f29e
hos <hovosanoyan@gmail.com>
2021-08-30 15:24:40 +0400

    Use process.env in next.config

--- next.config.js
+++ next.config.js
@@ -2,36 +2,36 @@ const { config } = require("dotenv");
 const { resolve } = require("path");
 
 const envPath = resolve(`./.env.local`);
-const env = config({ path: envPath }).parsed;
+config({ path: envPath });
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   env: {
-    IS_PROD: env.NODE_ENV === "production",
+    IS_PROD: process.env.NODE_ENV === "production",
 
-    MONGODB_URI: env.MONGODB_URI,
-    AMERIA_CLIENT_ID: env.AMERIA_CLIENT_ID,
-    AMERIA_UN: env.AMERIA_UN,
-    AMERIA_PW: env.AMERIA_PW,
-    AMERIA_IS_TEST: env.AMERIA_IS_TEST,
+    MONGODB_URI: process.env.MONGODB_URI,
+    AMERIA_CLIENT_ID: process.env.AMERIA_CLIENT_ID,
+    AMERIA_UN: process.env.AMERIA_UN,
+    AMERIA_PW: process.env.AMERIA_PW,
+    AMERIA_IS_TEST: process.env.AMERIA_IS_TEST,
 
-    HOST_URL: env.HOST_URL,
+    HOST_URL: process.env.HOST_URL,
 
-    SENDGRID_SECRET: env.SENDGRID_SECRET,
+    SENDGRID_SECRET: process.env.SENDGRID_SECRET,
 
     TELEGRAM: {
-      TOKEN: env.TELEGRAM_TOKEN,
-      CHAT_ID: env.TELEGRAM_CHAT_ID,
+      TOKEN: process.env.TELEGRAM_TOKEN,
+      CHAT_ID: process.env.TELEGRAM_CHAT_ID,
     },
 
-    PASSWORD: env.PASSWORD,
+    PASSWORD: process.env.PASSWORD,
 
     S3: {
-      ACCESS_KEY_ID: env.ACCESS_KEY_ID || "",
-      SECRET_ACCESS_KEY: env.SECRET_ACCESS_KEY || "",
-      ENDPOINT: env.S3_ENDPOINT || "",
-      REGION: env.S3_REGION || "eu-central-1",
-      BUCKET: env.BUCKET_NAME || "bohemnots",
+      ACCESS_KEY_ID: process.env.ACCESS_KEY_ID || "",
+      SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY || "",
+      ENDPOINT: process.env.S3_ENDPOINT || "",
+      REGION: process.env.S3_REGION || "eu-central-1",
+      BUCKET: process.env.BUCKET_NAME || "bohemnots",
     },
   },
   reactStrictMode: true,
