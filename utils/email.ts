d0a254b6f3d6bdd4cad0b7ac4f32a708bc3256ed
hos <hovosanoyan@gmail.com>
2021-08-31 19:59:23 +0400

    Log everything when sending email

--- utils/email.ts
+++ utils/email.ts
@@ -10,15 +10,24 @@ if (config.SENDGRID_SECRET) {
 const DENY_ID = "d-33026eebeee546f0b442194a5ab470c7";
 const APPROVE_ID = "d-a1ecd6ed7f184b3a83dbab0bbb3ff32a";
 
-export const sendDeny = (email: string, notes: string) => {
-  return SendGrid.send({
-    from: "bohemnotsradio@gmail.com",
-    to: email,
-    templateId: DENY_ID,
-    dynamicTemplateData: {
+export const sendDeny = async (email: string, notes: string) => {
+  try {
+    console.log("sending denying email to");
+    console.log({
+      email,
       notes,
-    },
-  });
+    });
+    await SendGrid.send({
+      from: "bohemnotsradio@gmail.com",
+      to: email,
+      templateId: DENY_ID,
+      dynamicTemplateData: {
+        notes,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const sendApprove = (
@@ -26,14 +35,24 @@ export const sendApprove = (
   checkoutId: string,
   notes: String
 ) => {
-  const qrCodeUrl = getTicketUrl(checkoutId);
-  return SendGrid.send({
-    from: "bohemnotsradio@gmail.com",
-    to: email,
-    templateId: APPROVE_ID,
-    dynamicTemplateData: {
+  try {
+    const qrCodeUrl = getTicketUrl(checkoutId);
+    console.log("sending approval email to");
+    console.log({
+      email,
       qrCodeUrl,
       notes,
-    },
-  });
+    });
+    return SendGrid.send({
+      from: "bohemnotsradio@gmail.com",
+      to: email,
+      templateId: APPROVE_ID,
+      dynamicTemplateData: {
+        qrCodeUrl,
+        notes,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
