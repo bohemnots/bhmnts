7fa79e01dddaafcb625cf1c1504c7e5b614f03c9
hos <44600044+hos@users.noreply.github.com>
2021-09-20 16:13:54 +0000

    Add Layout

--- pages/create-ticket.tsx
+++ pages/create-ticket.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import styled from "styled-components";
 import * as Yup from "yup";
 
-import { FestLayout } from "../components";
+import { FestLayout } from "../components/Layout";
 
 const initialValues = {
   name: "",
